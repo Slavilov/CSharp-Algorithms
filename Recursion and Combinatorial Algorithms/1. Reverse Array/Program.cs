b3a87b9d899ae3b47459cca8ee2b647849544707
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-12-20 23:43:43 +0200

    Add files via upload

---
+++ Recursion and Combinatorial Algorithms/1. Reverse Array/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace reverseArray
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            string[] arrayToBeReversed;
+            Console.WriteLine("Now type in the size of the Array!");
+            int n = int.Parse(Console.ReadLine());
+            arrayToBeReversed = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Now type in each element of the array!");
+                arrayToBeReversed[i] = Console.ReadLine();
+            }
+            ReverseArray(arrayToBeReversed, 0);
+
+        }
+        static void ReverseArray(string[] arrayToBeReversed, int indx)
+        {
+            if (indx >= arrayToBeReversed.Length/2)
+            {
+                Console.WriteLine(string.Join(", ", arrayToBeReversed));
+                return;
+            }
+
+            string tempElement = arrayToBeReversed[indx];
+            arrayToBeReversed[indx] = arrayToBeReversed[arrayToBeReversed.Length - (indx + 1)];
+            arrayToBeReversed[arrayToBeReversed.Length - (indx + 1)] = tempElement;
+
+            ReverseArray(arrayToBeReversed, indx+1);
+        }
+    }
+}
