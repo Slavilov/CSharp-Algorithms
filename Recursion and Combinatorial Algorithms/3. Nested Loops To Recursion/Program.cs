5cbd00a57865a388dd1f2c7013f113915b691c20
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-12-21 23:32:10 +0200

    Add files via upload

---
+++ Recursion and Combinatorial Algorithms/3. Nested Loops To Recursion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number of nested loops: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        SimulateLoops(arr, 0, n);
+    }
+
+    static void SimulateLoops(int[] arr, int depth, int n)
+    {
+        if (depth == n)
+        {
+            Console.WriteLine(string.Join(" ", arr));
+            return;
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            arr[depth] = i;
+            SimulateLoops(arr, depth + 1, n);
+        }
+    }
+}
