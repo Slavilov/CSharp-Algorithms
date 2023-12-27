aac6bd08a5e53434307c1c25a69c57275e028d9c
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-12-28 00:19:17 +0200

    Add files via upload

---
+++ Recursion and Combinatorial Algorithms/11. Recursion - Even and Odd Numbers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace addNumbersBackwards
+
+{
+    class Program
+
+    {
+
+        private static List<int> evenNumbers = new List<int>();
+        private static List<int> oddNumbers = new List<int>();
+        private static int startNumber = int.Parse(Console.ReadLine());
+        private static int endNumber = int.Parse(Console.ReadLine());
+        static void Main(string[] args)
+
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                numbers.Add(i);
+            }
+
+            GetSum(numbers, 0);
+        }
+        public static void GetSum(List<int> numbers, int index)
+        {
+            if (index == numbers.Count-1)
+            {
+                Console.WriteLine($"All even numbers from {startNumber} to {endNumber} are:");
+                Console.WriteLine(string.Join(", ", evenNumbers));
+
+                Console.WriteLine($"All odd numbers from {startNumber} to {endNumber} are:");
+                Console.WriteLine(string.Join(", ", oddNumbers));
+                return;
+            }
+            if (numbers[index]%2 == 0)
+            {
+                evenNumbers.Add(numbers[index]);
+            }
+            else
+            {
+                oddNumbers.Add(numbers[index]);
+            }
+            GetSum(numbers, index+1);
+        }
+    }
+}
