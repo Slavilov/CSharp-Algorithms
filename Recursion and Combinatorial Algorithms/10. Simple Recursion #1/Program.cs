f16e73aceba99799c73ef08aadf5793a3a168101
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-12-28 00:18:27 +0200

    Add files via upload

---
+++ Recursion and Combinatorial Algorithms/10. Simple Recursion #1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace addNumbersBackwards
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSum(n));
+        }
+        public static int GetSum(int n)
+        {
+            if (n ==1)
+            {
+                return 1;
+            }
+            return n+ GetSum(n - 1);
+        }
+    }
+}
