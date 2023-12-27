c7d11acb84d128f7eb098a9b331532e4e2e0f61b
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-12-27 16:55:47 +0200

    Add files via upload

---
+++ Recursion and Combinatorial Algorithms/9. Word Cruncher/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class WordCruncher
+{
+    private static List<string> inputWords;
+    private static string target;
+    private static LinkedList<string> selectedWords;
+    private static Dictionary<int, List<string>> wordsByIndex;
+    private static HashSet<string> result;
+
+    public static void Main()
+    {
+        inputWords = Console.ReadLine().Split(", ").ToList();
+        target = Console.ReadLine();
+
+        selectedWords = new LinkedList<string>();
+        wordsByIndex = new Dictionary<int, List<string>>();
+        result = new HashSet<string>();
+
+        foreach (var word in inputWords)
+        {
+            int index = target.IndexOf(word);
+            while (index != -1)
+            {
+                if (!wordsByIndex.ContainsKey(index))
+                {
+                    wordsByIndex[index] = new List<string>();
+                }
+
+                wordsByIndex[index].Add(word);
+                index = target.IndexOf(word, index + 1);
+            }
+        }
+
+        Generate(0);
+        Console.WriteLine(string.Join(Environment.NewLine, result));
+    }
+
+    private static void Generate(int index)
+    {
+        if (index >= target.Length)
+        {
+            result.Add(string.Join(" ", selectedWords));
+            return;
+        }
+
+        if (!wordsByIndex.ContainsKey(index)) return;
+
+        foreach (var word in wordsByIndex[index])
+        {
+            selectedWords.AddLast(word);
+            Generate(index + word.Length);
+            selectedWords.RemoveLast();
+        }
+    }
+}
