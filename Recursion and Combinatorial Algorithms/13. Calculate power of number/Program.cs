8d9615f90915f20a97262919da12fa2f3d7ea118
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-12-30 23:46:56 +0200

    Add files via upload

---
+++ Recursion and Combinatorial Algorithms/13. Calculate power of number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input the base value: ");
+        int baseValue = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input the exponent: ");
+        int exponent = Convert.ToInt32(Console.ReadLine());
+
+        int result = Power(baseValue, exponent);
+
+        Console.WriteLine($"The value of {0} to the power of {1} is: {2}", baseValue, exponent, result);
+    }
+
+    static int Power(int baseValue, int exponent)
+    {
+        if (exponent == 0)
+            return 1;
+        else
+            return baseValue * Power(baseValue, exponent - 1);
+    }
+}
