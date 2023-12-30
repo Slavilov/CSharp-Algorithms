414e49dcd4b3536d3b34e52cf4188ed955440c17
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-12-30 23:47:15 +0200

    Add files via upload

---
+++ Recursion and Combinatorial Algorithms/14. Reverse a string with Recursion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input the string: ");
+        string inputString = Console.ReadLine();
+
+        string reversedString = ReverseString(inputString);
+
+        Console.WriteLine("The reverse of the string is: " + reversedString);
+    }
+
+    // Function to reverse a string using recursion
+    static string ReverseString(string str)
+    {
+        if (str.Length <= 1)
+            return str;
+        else
+            return str[str.Length - 1] + ReverseString(str.Substring(0, str.Length - 1));
+    }
+}
