6567aeac03532f1f3ebecf73ba0e87da86c79bb2
Velichko Slavilov <velichkoslavilov98@gmail.com>
2024-01-02 20:02:43 +0200

    Add files via upload

---
+++ Recursion and Combinatorial Algorithms/16. Fibonacci Numbers Recursion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input number of terms for the Fibonacci series: ");
+        int terms = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("The Fibonacci series of " + terms + " terms is: ");
+        for (int i = 0; i < terms; i++)
+        {
+            Console.Write(Fibonacci(i) + " ");
+        }
+        Console.WriteLine();
+    }
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+            return n;
+        else
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
