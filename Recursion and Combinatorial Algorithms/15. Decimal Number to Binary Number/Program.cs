ffdeb8e2e31ae6a1e61193c72a4f7f3fcb063082
Velichko Slavilov <velichkoslavilov98@gmail.com>
2024-01-02 20:02:27 +0200

    Add files via upload

---
+++ Recursion and Combinatorial Algorithms/15. Decimal Number to Binary Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        Console.Write("Input a decimal number: ");
+        int decimalNumber = Convert.ToInt32(Console.ReadLine());
+
+        string binaryNumber = DecimalToBinary(decimalNumber);
+
+        Console.WriteLine("The binary equivalent of " + decimalNumber + " is: " + binaryNumber);
+    }
+
+    static string DecimalToBinary(int number)
+    {
+        if (number == 0)
+            return "";
+        else
+            return DecimalToBinary(number / 2) + (number % 2).ToString();
+    }
+}
