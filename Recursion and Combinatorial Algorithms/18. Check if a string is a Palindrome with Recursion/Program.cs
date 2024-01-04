af1c2a2dcf30ffa365631f774d46f031849e735f
Velichko Slavilov <velichkoslavilov98@gmail.com>
2024-01-04 23:36:24 +0200

    Add files via upload

---
+++ Recursion and Combinatorial Algorithms/18. Check if a string is a Palindrome with Recursion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input a string: ");
+        string inputString = Console.ReadLine();
+
+        if (IsPalindrome(inputString, 0, inputString.Length - 1))
+            Console.WriteLine("The string is Palindrome.");
+        else
+            Console.WriteLine("The string is not Palindrome.");
+    }
+    static bool IsPalindrome(string str, int start, int end)
+    {
+        if (start >= end)
+            return true;
+        if (str[start] != str[end])
+            return false;
+        return IsPalindrome(str, start + 1, end - 1);
+    }
+}
