e105bde1929369e1072cedd53ff1f18c767992aa
Velichko Slavilov <velichkoslavilov98@gmail.com>
2024-01-04 23:36:41 +0200

    Add files via upload

---
+++ Recursion and Combinatorial Algorithms/19. Calculate the LCM and GCD of two numbers using recursion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input the first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input the second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int gcd = GCD(num1, num2);
+        int lcm = LCM(num1, num2);
+
+        Console.WriteLine($"The GCD of {num1} and {num2} = {gcd}");
+        Console.WriteLine($"The LCM of {num1} and {num2} = {lcm}");
+    }
+    static int GCD(int a, int b)
+    {
+        if (b == 0)
+            return a;
+        else
+            return GCD(b, a % b);
+    }
+
+    static int LCM(int a, int b)
+    {
+        return (a * b) / GCD(a, b);
+    }
+}
