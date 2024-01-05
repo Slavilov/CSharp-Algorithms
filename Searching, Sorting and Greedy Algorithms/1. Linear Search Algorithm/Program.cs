eb300e6947dd1089ed58f28e90382f5b3edea976
Velichko Slavilov <velichkoslavilov98@gmail.com>
2024-01-05 23:17:47 +0200

    Add files via upload

---
+++ Searching, Sorting and Greedy Algorithms/1. Linear Search Algorithm/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] data = Console.ReadLine()
+            .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Console.WriteLine("Please type in what number you want to search for in the array:");
+        int x = int.Parse(Console.ReadLine());
+
+        int result = LinearSearch(data, x);
+        if (result == -1)
+            Console.WriteLine("Element not found");
+        else
+            Console.WriteLine("Element found at index: " + result);
+    }
+    static int LinearSearch(int[] arr, int x)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == x)
+                return i;
+        }
+        return -1;
+    }
+}
