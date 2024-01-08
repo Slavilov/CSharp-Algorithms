dec830026b7a78c83b99ca46f6fe48f4367589ea
Velichko Slavilov <velichkoslavilov98@gmail.com>
2024-01-08 19:16:27 +0200

    Add files via upload

---
+++ Searching, Sorting and Greedy Algorithms/4. Selection Sort Algorithm with Exercise/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] data = Console.ReadLine()
+            .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        Console.WriteLine("Original Array:");
+        PrintArray(data);
+
+        SelectionSort(data);
+
+        Console.WriteLine("Sorted Array:");
+        PrintArray(data);
+    }
+
+    static void SelectionSort(int[] arr)
+    {
+        int n = arr.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            // Find the minimum element in the unsorted array
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int item in arr)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
