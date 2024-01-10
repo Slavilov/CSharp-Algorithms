b339381ef8cc24a0f8b70df5e7dfc0cc0cabec59
Velichko Slavilov <velichkoslavilov98@gmail.com>
2024-01-10 13:11:56 +0200

    Add files via upload

---
+++ Searching, Sorting and Greedy Algorithms/6. Bubble Sort with Exercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] data = Console.ReadLine()
+            .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        Console.WriteLine("Original Array:");
+        PrintArray(data);
+
+        BubbleSort(data);
+
+        Console.WriteLine("Sorted Array:");
+        PrintArray(data);
+    }
+    static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int item in arr)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
