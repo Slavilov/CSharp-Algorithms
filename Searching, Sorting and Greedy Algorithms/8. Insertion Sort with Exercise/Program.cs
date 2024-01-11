7d34150fba1e4ba7d3b766f40fd397786c645634
Velichko Slavilov <velichkoslavilov98@gmail.com>
2024-01-11 13:01:02 +0200

    Add files via upload

---
+++ Searching, Sorting and Greedy Algorithms/8. Insertion Sort with Exercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] data = { 12, 11, 13, 5, 6 };
+        Console.WriteLine("Original Array:");
+        PrintArray(data);
+
+        InsertionSort(data);
+
+        Console.WriteLine("Sorted Array:");
+        PrintArray(data);
+    }
+    static void InsertionSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 1; i < n; ++i)
+        {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1], that are greater than key,
+            // to one position ahead of their current position
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    static void PrintArray(int[] arr)
+    {
+        foreach (int item in arr)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
