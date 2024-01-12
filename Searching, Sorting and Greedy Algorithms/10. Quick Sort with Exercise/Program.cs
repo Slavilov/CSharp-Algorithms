fa9f4f3f2b1e5ffc975aa74d7aaceebf66ab93d0
Velichko Slavilov <velichkoslavilov98@gmail.com>
2024-01-12 12:40:19 +0200

    Add files via upload

---
+++ Searching, Sorting and Greedy Algorithms/10. Quick Sort with Exercise/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] data = { 10, 7, 8, 9, 1, 5 };
+        int n = data.Length;
+        Console.WriteLine("Original Array:");
+        PrintArray(data);
+
+        QuickSort(data, 0, n - 1);
+
+        Console.WriteLine("Sorted Array:");
+        PrintArray(data);
+    }
+
+    static void QuickSort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            int pi = Partition(arr, low, high);
+
+            QuickSort(arr, low, pi - 1);
+            QuickSort(arr, pi + 1, high);
+        }
+    }
+    static int Partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp1 = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp1;
+
+        return i + 1;
+    }
+    static void PrintArray(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n; ++i)
+            Console.Write(arr[i] + " ");
+        Console.WriteLine();
+    }
+}
