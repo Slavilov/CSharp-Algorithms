a1d470db186df7a5bde308bde292d4763b88d8e9
Velichko Slavilov <velichkoslavilov98@gmail.com>
2024-01-14 12:19:42 +0200

    Add files via upload

---
+++ Searching, Sorting and Greedy Algorithms/12. Merge Sort with Exercise/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] data = Console.ReadLine()
+            .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        Console.WriteLine("Original Array:");
+        PrintArray(data);
+
+        MergeSort(data, 0, data.Length - 1);
+
+        Console.WriteLine("Sorted Array:");
+        PrintArray(data);
+    }
+    static void MergeSort(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int middle = left + (right - left) / 2;
+
+            MergeSort(arr, left, middle);
+            MergeSort(arr, middle + 1, right);
+
+            Merge(arr, left, middle, right);
+        }
+    }
+    static void Merge(int[] arr, int left, int middle, int right)
+    {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        int i, j;
+
+        for (i = 0; i < n1; ++i)
+            L[i] = arr[left + i];
+        for (j = 0; j < n2; ++j)
+            R[j] = arr[middle + 1 + j];
+
+        i = 0; j = 0;
+        int k = left;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    static void PrintArray(int[] arr)
+    {
+        foreach (int item in arr)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
